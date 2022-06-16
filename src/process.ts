5768124c8057d1ccdb41d0c1690dc840102c83bb
Tatiana Sapun <tatiana.sapun@gmail.com>
2022-06-16 17:29:06 +0300

    feat: implement request processor

--- src/process.ts
+++ src/process.ts
@@ -1,6 +1,10 @@
 import { IncomingMessage } from "http";
 import { URL } from "url";
-import { Answer, Person } from "./interfaces";
+import { Answer, Method, Person } from "./interfaces";
+import { get } from "./methods/get";
+import { post } from "./methods/post";
+import { put } from "./methods/put";
+import { del } from "./methods/delete";
 
 export class ServerAnswer implements Answer {
   private _status: number;
@@ -19,10 +23,53 @@ export class ServerAnswer implements Answer {
 
 const route = '/api/users';
 
-export const processRequest = (req: IncomingMessage): Answer => {
-  if (req.url === undefined) return new ServerAnswer(404, 'Not found');
-  const url: URL = new URL(req.url, `http://${req.headers.host}`);
-  if (!url.pathname.startsWith(route)) return new ServerAnswer (404, 'Not found');
-  const id: string = url.pathname.slice(route.length);
-  return new ServerAnswer(404, '');
+export class RequestProcessor {
+
+  public async execute(req: IncomingMessage): Promise<Answer> {
+    if (req.url === undefined) return new ServerAnswer(404, 'Resource that you requested does not exist');
+    const url: URL = new URL(req.url, `http://${req.headers.host}`);
+    if (!url.pathname.startsWith(route)) return new ServerAnswer (404, 'Resource that you requested does not exist');
+
+    const id: string = url.pathname.slice(route.length); 
+    const body: string= await this.getBody(req);
+
+    return this.processRequest(req.method, id, body);
+  }
+
+  private async getBody(req: IncomingMessage): Promise<string> {
+    return new Promise((resolve, reject) => {
+      let body = '';
+      req.on('data', (chunk: Buffer) => body += chunk.toString());
+      req.on('end', () => resolve(body));
+      req.on('error', (err: Error) => reject(err));
+    });
+  }
+  
+  private processRequest(method: string | undefined, id: string, body: string): Answer {
+    switch (method) {
+      case 'GET':
+        return this.runGetMethod(id);
+        break;
+      case 'POST':
+        return this.runPostMethod(body);
+        break;
+      case 'PUT':
+        return this.runPutMethod(id, body);
+        break;
+      case 'DELETE':
+        return this.runDeleteMethod(id);
+      default:
+        return this.runUnknownMethod();
+        break;
+    }
+  }
+
+  private runUnknownMethod(): Answer {
+    return new ServerAnswer(404, 'Method you are trying to execute is not found');
+  }
+
+  private runGetMethod: Method = get;
+  private runPostMethod: Method = post;
+  private runPutMethod: Method = put;
+  private runDeleteMethod: Method = del;
 }
