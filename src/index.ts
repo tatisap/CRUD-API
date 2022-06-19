d2822b617edbf62043692ae8cfd6f1dda6507d1a
Tatiana Sapun <tatiana.sapun@gmail.com>
2022-06-19 13:44:50 +0300

    feat: implement sending response

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,26 @@
 import http, { IncomingMessage, Server, ServerResponse } from 'http';
-import { database } from './db.js';
 import { Answer } from './interfaces.js';
-import { processRequest } from './process.js';
+import { RequestProcessor } from './process.js';
 
-const server: Server = http.createServer((req: IncomingMessage, res: ServerResponse): void => {
-  const answer: Answer = processRequest(req);
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify(database));
+export const server: Server = http.createServer(async (req: IncomingMessage, res: ServerResponse): Promise<void> => {
+  try {
+    const answer: Answer = await new RequestProcessor().execute(req);
+    res.writeHead(answer.status, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(answer.body));
+  }
+  catch (err) {
+    if (err instanceof Error) {
+      res.writeHead(500, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({
+        message: `${err.message}`,
+      }));
+    } else {
+      res.writeHead(500, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({
+        message: `Server does not answer`,
+    }));
+  }
+  }
 });
 
 server.listen(4000);
