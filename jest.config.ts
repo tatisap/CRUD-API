017542df127c60723450e9a2c77f0e1b2c11da81
Tatiana Sapun <tatiana.sapun@gmail.com>
2022-06-19 13:45:18 +0300

    feat: add jest config

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+import type { InitialOptionsTsJest } from 'ts-jest';
+const config: InitialOptionsTsJest = {
+  preset: 'ts-jest/presets/default-esm',
+  globals: {
+    'ts-jest': {
+      useESM: true
+    }
+  },
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+};
+export default config;
